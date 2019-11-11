1501a44a4925c185f501c3e179ad0ac50641d70c
Harm De Weirdt <harm.deweirdt@hogent.be>
2019-11-11 16:17:30 +0100

    chore: convert project build.gradle to kotlin DSL

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,13 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-
 buildscript {
-    ext.kotlin_version = '1.3.50'
     repositories {
         google()
         jcenter()
-        
+        maven { url = uri("https://jitpack.io") }
     }
     dependencies {
-        classpath 'com.android.tools.build:gradle:3.5.2'
-        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
+        classpath(BuildPlugins.androidGradlePlugin)
+        classpath(BuildPlugins.kotlinGradlePlugin)
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
@@ -19,10 +17,9 @@ allprojects {
     repositories {
         google()
         jcenter()
-        
+
     }
 }
-
-task clean(type: Delete) {
-    delete rootProject.buildDir
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
 }
