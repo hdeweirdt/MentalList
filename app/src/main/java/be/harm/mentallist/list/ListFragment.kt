90b2221c55b5ad826c405499a19afb52ba2e200a
Harm De Weirdt <harm.deweirdt@hogent.be>
2019-12-31 09:21:26 +0100

    feat: add divider between rv items

--- app/src/main/java/be/harm/mentallist/list/ListFragment.kt
+++ app/src/main/java/be/harm/mentallist/list/ListFragment.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import be.harm.mentallist.Injector
@@ -53,6 +54,10 @@ abstract class ListFragment : Fragment(), NewItemDialog.NewItemDialogListener {
 
         val swipeToDeleteHelper = ItemTouchHelper(SwipeToDeleteCallback(adapter, listViewModel))
         swipeToDeleteHelper.attachToRecyclerView(binding.rvItemList)
+
+        binding.rvItemList.addItemDecoration(
+            DividerItemDecoration(requireContext(), LinearLayoutManager.VERTICAL)
+        )
 
         listViewModel.items.observe(viewLifecycleOwner, Observer { itemList ->
             (binding.rvItemList.adapter as ItemListAdapter).submitList(itemList)
